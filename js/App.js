a8ed219cb82908eaf7c837000d02a1a0087f6918
EbukaNweje <nwejeebuka@gmail.com>
2022-04-21 15:41:17 -0700

    newpush

---
+++ js/App.js
@@ -0,0 +1,38 @@
+const AddTodo = document.getElementById("AddTodo")
+const TodoInput = document.getElementById("TodoInput")
+AddTodo.addEventListener("click", AddTodos)
+let AllTodo = []
+AllTodo = JSON.parse(localStorage.getItem("AllTodosInfo")) || []
+
+function AddTodos(e){
+    e.preventDefault()
+
+    AllTodo.push(TodoInput.value)
+    localStorage.setItem("AllTodosInfo", JSON.stringify(AllTodo))
+    window.location.reload()
+}
+
+const TodoOutput = document.querySelector(".TodoOutput")
+AllTodo = AllTodo.forEach((todos, index) => {   
+    const render = `
+    <div class="OutPutDiv">
+        <div><input type="checkbox" name="CheckBox" id="checkbox"></div>
+        <div> <input type="text" id="TodoText" value="${todos}"></div>
+        <div class="Edit"><i class="fa fa-pencil" aria-hidden="true"></i></div>
+        <div class="Delete"><i id=${index} class="fa fa-trash Dell" aria-hidden="true"></i></div>
+    </div>
+    `
+    // console.log(AddTodo)
+    TodoOutput.innerHTML += render
+});
+
+
+const deletes = document.querySelectorAll(".Delete")
+deletes.forEach((Deletes, index)=>{
+      Deletes.addEventListener("click", ()=>{
+            AllTodo = AllTodo.filter((book, i)=> index !== i)
+            localStorage.setItem("AllTodosInfo", JSON.stringify(AllTodo))
+            window.location.reload()
+            console.log(deletes);
+      })       
+})
