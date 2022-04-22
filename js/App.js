0ee46d28a4a50aa2a7906660618bff01e1ee352d
EbukaNweje <nwejeebuka@gmail.com>
2022-04-21 21:03:29 -0700

    th

--- js/App.js
+++ js/App.js
@@ -1,5 +1,5 @@
 const AddTodo = document.getElementById("AddTodo")
-const TodoInput = document.getElementById("TodoInput")
+let TodoInput = document.getElementById("TodoInput")
 AddTodo.addEventListener("click", AddTodos)
 let AllTodo = []
 AllTodo = JSON.parse(localStorage.getItem("AllTodosInfo")) || []
@@ -16,8 +16,8 @@ const TodoOutput = document.querySelector(".TodoOutput")
     AllTodo.forEach((todos, index) => {   
     const render = `
     <div class="OutPutDiv">
-        <div><input type="checkbox" name="CheckBox" id="checkbox"></div>
-        <div> <input type="text" id="TodoText" value="${todos}"></div>
+        <div><input type="checkbox" name="CheckBox" id=${index} class="checkbox"></div>
+        <div> <input type="text" class="TodoText" id="TodoText${index}" value="${todos}"></div>
         <div class="Edit"><i class="fa fa-pencil" aria-hidden="true"></i></div>
         <button class="Delete" id=${index}><i class="fa fa-trash Dell" aria-hidden="true"></i></button>
     </div>
@@ -36,3 +36,28 @@ deletes.forEach((Deletes, index)=>{
             console.log(deletes);
       })       
 })
+// 'input[type=checkbox]'
+
+const Checkme = ()=>{
+    const checkbox = document.querySelectorAll('input[type=checkbox]')
+checkbox.forEach((check)=>{
+    check.addEventListener('change', () => {
+        if(check.checked) {
+            TodoText = document.getElementById("TodoText")
+            TodoText.style.textDecoration = "line-through"
+        } else {
+            TodoText = document.getElementById("TodoText")
+            TodoText.style.textDecoration = "none"
+        }
+      });
+})
+}
+Checkme()
+
+const edit = document.querySelectorAll('.fa.fa-pencil');
+edit.forEach((edits, index) => edits.addEventListener('click', () => editTask(AllTodo, index)));
+function editTask(AllTodo, index){
+    AllTodo[index] = document.getElementById(`TodoText${index}`).value
+    localStorage.setItem("AllTodosInfo", JSON.stringify(AllTodo))
+    window.location.reload()
+}
